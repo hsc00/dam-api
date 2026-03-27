93cee6584a6c512ee8901d2c0f244169ef101a47
hsc00 <hscoutinho00@gmail.com>
2026-03-27 13:51:53 +0000

    refactor: using UUID v4

--- src/Domain/Asset/ValueObject/UploadId.php
+++ src/Domain/Asset/ValueObject/UploadId.php
@@ -6,7 +6,7 @@ namespace App\Domain\Asset\ValueObject;
 
 final readonly class UploadId
 {
-    private const UUID_PATTERN = '/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-8][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/';
+    private const UUID_PATTERN = '/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/';
 
     /**
      * @throws \InvalidArgumentException
