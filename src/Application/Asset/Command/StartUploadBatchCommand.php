2c97f4638dd6636105fb16bb290092e1f127ab16
hsc00 <hscoutinho00@gmail.com>
2026-04-09 22:41:18 +0100

    fix(command): validate StartUploadBatchCommand:: is list<StartUploadBatchFileCommand> (use get_debug_type for diagnostics)

--- src/Application/Asset/Command/StartUploadBatchCommand.php
+++ src/Application/Asset/Command/StartUploadBatchCommand.php
@@ -13,5 +13,18 @@ final readonly class StartUploadBatchCommand
         public string $accountId,
         public array $files,
     ) {
+        // Validate that $files is a sequential list and contains only
+        // StartUploadBatchFileCommand instances. Fail fast for invalid payloads.
+        if (! array_is_list($this->files)) {
+            throw new \InvalidArgumentException('StartUploadBatchCommand::$files must be a sequential list (list<StartUploadBatchFileCommand>).');
+        }
+
+        foreach ($this->files as $i => $item) {
+            if (! $item instanceof StartUploadBatchFileCommand) {
+                $given = \get_debug_type($item);
+
+                throw new \InvalidArgumentException(sprintf('StartUploadBatchCommand::$files[%d] must be instance of StartUploadBatchFileCommand, %s given', $i, $given));
+            }
+        }
     }
 }
